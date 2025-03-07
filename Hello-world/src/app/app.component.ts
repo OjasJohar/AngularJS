ca6980b5cc94e0e042a2a87ea46ec1f2b90e861a
OjasJohar <ojasjohar@gmail.com>
2025-03-07 16:17:21 +0530

    [Ojas] Add. Validation for userName and display Error

--- Hello-world/src/app/app.component.ts
+++ Hello-world/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   imgUrl="../assets/BL_logo_square_jpg.jpg";
   url : string="https://www.bridgelabz.com";
   userName: string ="";
+  nameError: string ="";
 
   ngOnInit(): void{
     this.title="Hello from BridgeLabz.";
@@ -21,6 +22,16 @@ export class AppComponent {
 
   onClick($event: Event){
     console.log("Save button is clicked!", $event);
-    window.open(this.url, "-blank");
+    window.open(this.url, "_blank");
+  }
+
+  onInput(){
+    console.log("Change Event Occured!");
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!";
   }
 }

4ff8b7ee7ea3578e9996c1a1f71a093686b55ae0
OjasJohar <ojasjohar@gmail.com>
2025-03-07 15:32:30 +0530

    [Ojas] Add. Event Binding Technique to bind Click Event

--- Hello-world/src/app/app.component.ts
+++ Hello-world/src/app/app.component.ts
@@ -11,8 +11,14 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'Hello-world';
   imgUrl="../assets/BL_logo_square_jpg.jpg";
+  url : string="https://www.bridgelabz.com";
 
   ngOnInit(): void{
     this.title="Hello from BridgeLabz.";
+  }
+
+  onClick($event: Event){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "-blank");
   }
 }
